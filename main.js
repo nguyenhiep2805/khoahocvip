acbb855ef3bc63d3ddd1b84e34c03152531b304a
Nguyen Hiep <nguyenhiep.190217@gmail.com>
2022-11-30 01:18:01 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+// Change navbar styles on scroll
+
+window.addEventListener('scroll', () => {
+    document
+        .querySelector('nav')
+        .classList.toggle('window-scroll', window.scrollY > 100);
+});
+
+// Show or hide FAQ answer
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach((faq) => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // Change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if (icon.className === 'uil uil-plus') {
+            icon.className = 'uil uil-minus';
+        } else {
+            icon.className = 'uil uil-plus';
+        }
+    });
+});
+
+// Swiper JS
+new Swiper('.mySwiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    // When window width is >= 600px
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+        },
+    },
+});
+
+// Show/ Hide Nav menu
+const menu = document.querySelector('.nav__menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'flex';
+    closeBtn.style.display = 'inline-block';
+    menuBtn.style.display = 'none';
+});
+
+// Close Nav menu
+const closeNav = () => {
+    menu.style.display = 'none';
+    closeBtn.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+};
+
+closeBtn.addEventListener('click', closeNav);
